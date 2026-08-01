e01ca26d80507c366e092dbbb3bffcdfce1da576
Pooja <cchinki99@gmail.com>
2026-08-01 21:34:37 +0530

    Fix Docker and Remotion rendering

--- remotion-project/remotion.config.ts
+++ remotion-project/remotion.config.ts
@@ -3,9 +3,11 @@ import {enableTailwind} from "@remotion/tailwind-v4";
 
 Config.setVideoImageFormat("jpeg");
 Config.setOverwriteOutput(true);
-Config.overrideWebpackConfig(enableTailwind);
 
-// ADD THESE
 Config.setBrowserExecutable("/usr/bin/chromium");
+
 Config.setChromiumDisableWebSecurity(true);
+
 Config.setDelayRenderTimeoutInMilliseconds(120000);
+
+Config.overrideWebpackConfig(enableTailwind);
